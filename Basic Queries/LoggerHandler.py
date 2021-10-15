e2e47fb54ce12cf10373691d0cc9c4e2dacb18a7
MOHAMMAD-FATHA <mfjkd7795@gmail.com>
2021-10-16 02:39:42 +0530

    [Mohammad] Add: Log Handler file

---
+++ Basic Queries/LoggerHandler.py
@@ -0,0 +1,8 @@
+import logging
+
+logger = logging
+
+logger.basicConfig(filename="/home/fatha/Documents/MySql/Basic Queries/mysqloper.log'",level=logging.INFO, format='%(asctime)s:%(funcName)s:%(levelname)s:%(message)s')
+
+logger.basicConfig(filename='/home/fatha/Documents/MySql/Basic Queries/mysqloper.log',
+                    level=logging.ERROR, format='%(asctime)s:%(funcName)s:%(levelname)s:%(lineo)d')
